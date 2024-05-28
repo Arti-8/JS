30f91303b5d037ada85b319b5f9036a7bc1fa388
Arti Rani <raniarti2002@gmail.com>
2024-05-28 17:15:34 +0530

    function date

---
+++ Basics/strings.js
@@ -0,0 +1,2 @@
+console.log(1+"2"+"3")
+console.log("2"+"2"+1+4)
--- Basics/variables.js
+++ Basics/variables.js
@@ -16,26 +16,3 @@ console.table([accCity,accEmail,accId])
 console.log(x)
 var x = 2
 console.log(x)
-
-/*
-let number = 50
-
-function print() {
-  let square = number * number
-
-  if (number < 60) {
-    var largerNumber = 80
-    let anotherLargerNumber = 100
-
-    console.log(square)
-  }
-
-  console.log(largerNumber)
-  console.log(anotherLargerNumber)
-}
-
-print()
-// 2500
-// 80
-// ReferenceError: anotherLargerNumber is not defined
-*/
